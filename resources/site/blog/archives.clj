2b6946a61379e2efda895935efc09b62ae761f41
Alexander Yakushev <alex@bytopia.org>
2014-08-11 20:41:53 +0300

    Update blog template

--- resources/site/blog/archives.clj
+++ resources/site/blog/archives.clj
@@ -1,17 +1,15 @@
 {:title "Archives"
  :show-blog-nav true}
 
-[:div {:class "container"}
+[:div.container
  [:p]
- [:ul {:class "posts"}
+ [:ul.posts
   (for [f (reverse (io/list-files :posts))
         :let [url (post-url f)
               [metadata content] (io/read-doc f)]]
     [:li {:style "list-style: none outside none;"}
      [:div
       [:a {:href url} (:title metadata)]
-      [:span {:style "float:right"
-              :class "entry-date"} (format-date (:date metadata) "dd.MM.YYYY")]
-      [:div {:class "clear"}]]
-
-     ])]]
+      [:span.entry-date {:style "float:right"}
+       (format-date (:date metadata) "dd.MM.YYYY")]
+      [:div.clear]]])]]
--- resources/site/blog/index.clj
+++ resources/site/blog/index.clj
@@ -1,22 +1,22 @@
 {:title "Blog"
  :show-blog-nav true}
 
-[:div {:class "container"}
+[:div.container
  (for [f (take 5 (reverse (io/list-files :posts)))
        :let [url (post-url f)
              [metadata content] (io/read-doc f)]]
    [:div
-    [:div {:class "entry-title"}
+    [:div.entry-title
      [:h2 {:style "float:left"}
       [:a {:href url} (:title metadata)]]
-     [:span {:style "float:right"
-             :class "entry-date"} (format-date (:date metadata) "dd.MM.YYYY")]
-     [:div {:class "clear"}]]
+     [:span.entry-date {:style "float:right"}
+      (format-date (:date metadata) "dd.MM.YYYY")]
+     [:div.clear]]
 
-    [:div {:class "entry-content"}
+    [:div.entry-content
      (if-let [synopsis (:synopsis metadata)]
        (list synopsis
-             [:p {:class "readmore"}
+             [:p.readmore
               [:a {:href url} "Read more ->>"]])
        content)]])
  [:p "For more posts check out the " [:a {:href "/blog/archives.html"} "Archives"] " Web page >>"]]
