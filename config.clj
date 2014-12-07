78d0cfe347ddadfde91ea55bfddb881442b42fe1
Alexander Yakushev <alex@bytopia.org>
2014-12-07 10:37:55 +0100

    New posts and fixes

--- config.clj
+++ config.clj
@@ -18,4 +18,17 @@
  :post-deploy-cmd ["ssh" "digitalocean-bytopia" "/home/www-data/deploy-clojure-android"]
 
  :emacs "/usr/bin/emacs"
- :emacs-eval [(require 'org)]]
+ :emacs-eval [(require 'org) (require 'ox-html) (require 'cl)
+              (setq org-export-with-section-numbers nil)
+              (setq org-html-footnotes-section "<div id=\"footnotes\">
+<p style=\"display:none;\">%s</p>%s</div>")
+              (defun org-html-format-footnote-definition (fn)
+                (let ((n (car fn)) (def (cdr fn)))
+                  (format
+                   "<div class=\"footdef\">%s %s</div>\n"
+                   (format org-html-footnote-format
+                           (let* ((id (format "fn.%s" n))
+                                  (href (format " href=\"#fnr.%s\"" n))
+                                  (attributes (concat " class=\"footnum\"" href)))
+                                 (org-html--anchor id n attributes)))
+                   (substring def 21))))]]
